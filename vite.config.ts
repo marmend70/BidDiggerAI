a95e45301b3d8168e9df80c39e6f1f6bf7f2d0e3
marmend70 <mario.menditto@gmail.com>
2025-12-17 23:06:43 +0100

    17122025 FIXED MASTER

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
     resolve: {
